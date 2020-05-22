b27e943908d00ef886c762dfb9a54d0a0e072f32
kk-18 <46083260+kk-18@users.noreply.github.com>
2020-05-22 16:29:05 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,15 +1,4 @@
 
 ENVDICT = {
-    'app': {
-        'ip': 'https://applettest.xiaochebai.com'
-    },
-    'cabinet': {
-        'ip': 'https://cabinettest.xiaochebai.com'
-    },
-    'middle': {
-        'ip': 'http://114.116.136.190:12002'
-    },
-    'callback': {
-        'ip': 'https://callbacktest.xiaochebai.com'
-    }
+    
         }
